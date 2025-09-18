08a72f5cfb2d3afb315298d05e7a89e0a40d4b78
pixelated21 <javaughnbailey21@gmail.com>
2025-09-17 21:04:02 -0500

    Add @elysiajs/html and @kitajs/ts-html-plugin dependencies; implement misc router and page for health check and version endpoints; update TypeScript configuration for JSX support.

---
+++ apps/server/src/module/misc/page.tsx
@@ -0,0 +1,10 @@
+import { Html } from "@elysiajs/html";
+
+export const MiscPage = () => {
+    return (
+        <html lang="en">
+            <h1>Welcome to the Broken Experiences API</h1>
+            <a href="/openapi">API Documentation</a>
+        </html>
+    );
+};
---
+++ apps/server/src/module/misc/router.ts
@@ -0,0 +1,17 @@
+import { html } from '@elysiajs/html';
+import Elysia from "elysia";
+import { MiscPage } from './page';
+
+export const miscRouter = new Elysia()
+    .use(html())
+    .get('/', ({html}) => html(MiscPage()))
+    .get("/health", () => {
+        return {
+            status: "ok",
+        };
+    })
+    .get("/version", () => {
+        return {
+            version: "1.0.0",
+        };
+    });
