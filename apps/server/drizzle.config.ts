34f12d2201ada84a2edd91113c21bbe23ca71c8b
pixelated21 <javaughnbailey21@gmail.com>
2025-09-29 14:21:56 -0500

    feat: update drizzle configuration to use environment variables, enable SSL, and enhance logging and strictness

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,10 +1,17 @@
+import { env } from "@server/env";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env" });
 
 export default defineConfig({
 	schema: "./src/db/schema",
 	out: "./src/db/migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: process.env.DATABASE_URL || "",
+		url: env.DATABASE_URL,
+		ssl: 'require'
 	},
+	verbose: true,
+	strict: true,
 });
--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,7 +1,10 @@
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from 'postgres'
+import postgres from "postgres";
 import { env } from "../env";
 import * as schema from "./schema";
 
+config({ path: ".env" });
+
 const client = postgres(env.DATABASE_URL, { prepare: false })
-export const db = drizzle(client, { schema});
+;export const db = drizzle(client, { schema} );
