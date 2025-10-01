1fc6dc82286cae5506b1a205bd65327eae360d67
Jason Gayle <jasonblue1626@gmail.com>
2025-10-01 10:51:57 -0500

    chore: removed unused imports

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
 // Import the generated route tree
