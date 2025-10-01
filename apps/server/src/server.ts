58d6bf76900d5e047eebf44d76ffe0501bae7120
Jason Gayle <jasonblue1626@gmail.com>
2025-09-30 19:34:24 -0500

    merge: merged from main

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -31,11 +31,7 @@ export const app = new Elysia()
 	.use(logixlysia())
 	.use(
 		cors({
-<<<<<<< HEAD
-			origin: ["http://localhost:3001", "http://localhost:3000", "http://localhost:3002"],
-=======
 			origin: corsOrigins,
->>>>>>> 7390103f0aca37d6f39f4a276f9947abe0764f36
 			methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
 			allowedHeaders: ["Content-Type", "Authorization"],
 			credentials: true,
